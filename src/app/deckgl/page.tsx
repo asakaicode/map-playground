8d7524c3a6368d06522fcfa055e38893329c47ad
asakaicode <tubakaito5100@gmail.com>
2024-04-08 19:08:35 +0900

    feat: add token and style

--- src/app/deckgl/page.tsx
+++ src/app/deckgl/page.tsx
@@ -1,12 +1,16 @@
 'use client'
 
-import { DeckGL, MapView } from "deck.gl"
+import { DeckGL } from "deck.gl"
+import { Map } from "react-map-gl"
+
+const MAP_STYLE = 'mapbox://styles/asakaicode/cliim7tuf004l01r70clda6td'
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYXNha2FpY29kZSIsImEiOiJjbGQ0N3F4NG8wcTV0M3FqcjczZW9iY3A4In0.-pqoKHyEoBvT0-bmxa0a3A'
 
 export default function DeckGLPage() {
   return (
     <>
-      <DeckGL>
-        <MapView controller={true} />
+      <DeckGL controller={true}>
+        <Map mapStyle={MAP_STYLE} mapboxAccessToken={MAPBOX_ACCESS_TOKEN} />
       </DeckGL>
     </>
   )
