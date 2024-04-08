e63bea93532c7d345a4f8d241554cf8611066e62
asakaicode <tubakaito5100@gmail.com>
2024-04-08 18:56:08 +0900

    feat: add new page of deck.gl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+'use client'
+
+import { Button, Stack } from "@mui/material";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter()
   return (
     <div>
       <h1>Map Playground</h1>
-      
+      <Stack spacing={2} direction='row' >
+        <Button variant='contained' color='primary' onClick={() => router.push('/deckgl')}>Deck.gl</Button>
+        <Button variant='contained' color='primary'>Mapbox GL JS</Button>
+        <Button variant='contained' color='primary'>Leaflet</Button>
+      </Stack>
     </div> 
   )
 }
