e63acf0be6b64943d1c71c8fa909f24865e519eb
asakaicode <tubakaito5100@gmail.com>
2024-04-09 19:00:35 +0900

    fix: use singleQuote

--- src/app/deckgl/page.tsx
+++ src/app/deckgl/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import { DeckGL } from "deck.gl"
-import { Map } from "react-map-gl"
+import { DeckGL } from 'deck.gl'
+import { Map } from 'react-map-gl'
 
 const MAP_STYLE = 'mapbox://styles/asakaicode/cliim7tuf004l01r70clda6td'
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYXNha2FpY29kZSIsImEiOiJjbGQ0N3F4NG8wcTV0M3FqcjczZW9iY3A4In0.-pqoKHyEoBvT0-bmxa0a3A'
 
 export default function DeckGLPage() {
+
   return (
     <>
       <DeckGL controller={true}>
