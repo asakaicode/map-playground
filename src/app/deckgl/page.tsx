379a2dfc5c97dd9938ec47c6b04c613ef6eda124
asakaicode <tubakaito5100@gmail.com>
2024-04-09 19:15:17 +0900

    feat: adapt layers

--- src/app/deckgl/page.tsx
+++ src/app/deckgl/page.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import { DETOUR_1, DETOUR_2, DETOUR_3, NEAR_HIGASHI_NIHOMBASHI, NEAR_ME } from '@/modules/deckgl/helpers/consts'
+import { generatePathLayer, generatePathLayerObject } from '@/modules/deckgl/helpers/layer'
+import { colors } from '@mui/material'
 import { DeckGL } from 'deck.gl'
 import { Map } from 'react-map-gl'
 
@@ -7,10 +10,18 @@ const MAP_STYLE = 'mapbox://styles/asakaicode/cliim7tuf004l01r70clda6td'
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYXNha2FpY29kZSIsImEiOiJjbGQ0N3F4NG8wcTV0M3FqcjczZW9iY3A4In0.-pqoKHyEoBvT0-bmxa0a3A'
 
 export default function DeckGLPage() {
-
+  const coords = [NEAR_ME, DETOUR_1, DETOUR_2, DETOUR_3, NEAR_HIGASHI_NIHOMBASHI]
+  const initialViewState = {
+    longitude: NEAR_ME.lng,
+    latitude: NEAR_ME.lat,
+    zoom: 11,
+  }
+  const mainColor = colors.deepPurple.A700
+  const pathLayerObject = generatePathLayerObject(mainColor, coords)
+  const pathLayer = generatePathLayer([pathLayerObject])
   return (
     <>
-      <DeckGL controller={true}>
+      <DeckGL controller={true} layers={[pathLayer]}>
         <Map mapStyle={MAP_STYLE} mapboxAccessToken={MAPBOX_ACCESS_TOKEN} />
       </DeckGL>
     </>
